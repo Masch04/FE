488f6d388f0f992af4b2db55a7180d6f9d725851
Masch04 <masch04@gmail.com>
2025-11-19 21:39:13 +0700

    sua view ad

--- src/layout/components/MenuRocker.vue
+++ src/layout/components/MenuRocker.vue
@@ -1,187 +1,145 @@
 <template>
     <div class="nav-container primary-menu">
-        <div class="mobile-topbar-header">
-            <div>
-                <img src="../../assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
-            </div>
-            <div>
-                <h4 class="logo-text">Rukada</h4>
-            </div>
-            <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
-            </div>
+      <div class="mobile-topbar-header">
+        <div>
+          <img src="../../assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
         </div>
-        <nav class="navbar navbar-expand-xl w-100">
-            <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
-                <li class="nav-item dropdown">
-                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
-                        data-bs-toggle="dropdown">
-                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
-                        </div>
-                        <div class="menu-title text-nowrap">Dashboard</div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li> <a class="dropdown-item" href="index.html"><i class="bx bx-right-arrow-alt"></i>Default</a>
-                        </li>
-                        <li> <a class="dropdown-item" href="index2.html"><i
-                                    class="bx bx-right-arrow-alt"></i>Alternate</a>
-                        </li>
-                        <li> <a class="dropdown-item" href="index3.html"><i
-                                    class="bx bx-right-arrow-alt"></i>Graphical</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/admin/loai-phong">
-                        <a class="nav-link" href="/admin/loai-phong">
-                            <div class="parent-icon"><i class="fa-solid fa-chess-rook"></i>
-                            </div>
-                            <div class="menu-title text-nowrap">Loại Phòng</div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/admin/dich-vu">
-                        <a class="nav-link" href="/admin/dich-vu">
-                            <div class="parent-icon"><i class="fa-brands fa-servicestack"></i>
-                            </div>
-                            <div class="menu-title text-nowrap">Dịch Vụ</div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item dropdown">
-                    <router-link to="/admin/quan-ly-phong">
-                        <a class="nav-link" href="/admin/quan-ly-phong">
-                            <div class="parent-icon"><i class="fa-solid fa-hotel"></i>
-                            </div>
-                            <div class="menu-title text-nowrap">Quản Lý Phòng</div>
-                        </a>
-                    </router-link>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <router-link to="/admin/chi-tiet-thue-phong">
-                                <a class="dropdown-item" href="/admin/chi-tiet-thue-phong">
-                                    <i class="bx bx-right-arrow-alt"></i>Chi tiết thuê phòng</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/tinh-trang-phong">
-                                <a class="dropdown-item" href="/admin/tinh-trang-phong">
-                                    <i class="bx bx-right-arrow-alt"></i>Tình trạng phòng</a>
-                            </router-link>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
-                        data-bs-toggle="dropdown">
-                        <div class="parent-icon"><i class="fa-solid fa-users"></i>
-                        </div>
-                        <div class="menu-title">Quản Lý Người Dùng</div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <router-link to="/admin/nhan-vien">
-                                <a class="dropdown-item" href="/admin/nhan-vien">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Nhân Viên</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/khach-hang">
-                                <a class="dropdown-item" href="/admin/khach-hang">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Khách Hàng</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/phan-quyen">
-                                <a class="dropdown-item" href="/admin/phan-quyen">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Phân Quyền</a>
-                            </router-link>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
-                        data-bs-toggle="dropdown">
-                        <div class="parent-icon"><i class="fa-solid fa-ranking-star"></i>
-                        </div>
-                        <div class="menu-title">Quản Lý HomePage</div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <router-link to="/admin/slide">
-                                <a class="dropdown-item" href="/admin/slide">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Slide</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/review">
-                                <a class="dropdown-item" href="/admin/review">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Review</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/chuyen-muc">
-                                <a class="dropdown-item" href="/admin/chuyen-muc">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Chuyên Mục</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/bai-viet">
-                                <a class="dropdown-item" href="/admin/bai-viet">
-                                    <i class="bx bx-right-arrow-alt"></i>Quản Lý Bài Viết</a>
-                            </router-link>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/admin/danh-sach-hoa-don">
-                        <a class="nav-link" href="/admin/danh-sach-hoa-don">
-                            <div class="parent-icon"><i class="fa-solid fa-file-invoice-dollar"></i>
-                            </div>
-                            <div class="menu-title text-nowrap">Danh Sách Hóa Đơn </div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link">
-                        <div class="parent-icon"><i class="fa-solid fa-chart-column"></i>
-                        </div>
-                        <div class="menu-title text-nowrap">Thống Kê</div>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <router-link to="/admin/thong-ke-1">
-                                <a class="dropdown-item" href="/admin/thong-ke-1">
-                                    <i class="bx bx-right-arrow-alt"></i>Thống Kê Doanh Thu</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/thong-ke-2">
-                                <a class="dropdown-item" href="/admin/thong-ke-2">
-                                    <i class="bx bx-right-arrow-alt"></i>Thống Kê Số Lượng Phòng</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/thong-ke-3">
-                                <a class="dropdown-item" href="/admin/thong-ke-3">
-                                    <i class="bx bx-right-arrow-alt"></i>Thống Kê Tổng Tiền Khách Thanh Toán</a>
-                            </router-link>
-                        </li>
-                        <li>
-                            <router-link to="/admin/thong-ke-4">
-                                <a class="dropdown-item" href="/admin/thong-ke-4">
-                                    <i class="bx bx-right-arrow-alt"></i>Thống Kê Số Phòng Thuê Của Khách</a>
-                            </router-link>
-                        </li>
-                    </ul>
-                </li>
+        <div>
+          <h4 class="logo-text">Rukada</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i></div>
+      </div>
+  
+      <nav class="navbar navbar-expand-xl w-100">
+        <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
+  
+          <!-- Loại Phòng -->
+          <li class="nav-item">
+            <router-link to="/admin/loai-phong" class="nav-link">
+              <div class="menu-title text-nowrap">Loại Phòng</div>
+            </router-link>
+          </li>
+  
+          <!-- Dịch Vụ -->
+          <li class="nav-item">
+            <router-link to="/admin/dich-vu" class="nav-link">
+              <div class="menu-title text-nowrap">Dịch Vụ</div>
+            </router-link>
+          </li>
+  
+          <!-- Quản Lý Phòng -->
+          <li class="nav-item dropdown">
+            <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
+              <div class="menu-title text-nowrap">Quản Lý Phòng</div>
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link to="/admin/chi-tiet-thue-phong" class="dropdown-item">Chi tiết thuê phòng</router-link></li>
+              <li><router-link to="/admin/tinh-trang-phong" class="dropdown-item">Tình trạng phòng</router-link></li>
             </ul>
-        </nav>
+          </li>
+  
+          <!-- Quản Lý Người Dùng -->
+          <li class="nav-item dropdown">
+            <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
+              <div class="menu-title">Quản Lý Người Dùng</div>
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link to="/admin/nhan-vien" class="dropdown-item">Quản Lý Nhân Viên</router-link></li>
+              <li><router-link to="/admin/khach-hang" class="dropdown-item">Quản Lý Khách Hàng</router-link></li>
+              <li><router-link to="/admin/phan-quyen" class="dropdown-item">Quản Lý Phân Quyền</router-link></li>
+            </ul>
+          </li>
+  
+          <!-- Quản Lý HomePage -->
+          <li class="nav-item dropdown">
+            <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
+              <div class="menu-title">Quản Lý HomePage</div>
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link to="/admin/slide" class="dropdown-item">Quản Lý Slide</router-link></li>
+              <li><router-link to="/admin/review" class="dropdown-item">Quản Lý Review</router-link></li>
+              <li><router-link to="/admin/chuyen-muc" class="dropdown-item">Quản Lý Chuyên Mục</router-link></li>
+              <li><router-link to="/admin/bai-viet" class="dropdown-item">Quản Lý Bài Viết</router-link></li>
+            </ul>
+          </li>
+  
+          <!-- Danh Sách Hóa Đơn -->
+          <li class="nav-item">
+            <router-link to="/admin/danh-sach-hoa-don" class="nav-link">
+              <div class="menu-title text-nowrap">Danh Sách Hóa Đơn</div>
+            </router-link>
+          </li>
+  
+          <!-- Thống Kê -->
+          <li class="nav-item dropdown">
+            <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
+              <div class="menu-title text-nowrap">Thống Kê</div>
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link to="/admin/thong-ke-1" class="dropdown-item">Thống Kê Doanh Thu</router-link></li>
+              <li><router-link to="/admin/thong-ke-2" class="dropdown-item">Thống Kê Số Lượng Phòng</router-link></li>
+              <li><router-link to="/admin/thong-ke-3" class="dropdown-item">Thống Kê Tổng Tiền Khách Thanh Toán</router-link></li>
+              <li><router-link to="/admin/thong-ke-4" class="dropdown-item">Thống Kê Số Phòng Thuê Của Khách</router-link></li>
+            </ul>
+          </li>
+  
+        </ul>
+      </nav>
     </div>
-</template>
-<script>
-export default {
-
-}
-</script>
-<style></style>
+  </template>
+  
+  <script>
+  export default {}
+  </script>
+  
+  <style scoped>
+  /* Bỏ hoàn toàn icon (không có .parent-icon nữa) */
+  .parent-icon {
+    display: none !important;
+  }
+  
+  /* Bỏ gạch xanh (active indicator) dưới menu */
+  .navbar-nav .nav-link::after,
+  .navbar-nav .dropdown-toggle::after {
+    display: none !important;
+  }
+  
+  /* Căn giữa chữ trong menu */
+  .nav-link {
+    padding-left: 1.5rem !important;
+    padding-right: 1.5rem !important;
+  }
+  
+  /* Hover nhẹ cho menu */
+  .nav-link:hover,
+  .dropdown-item:hover {
+    background-color: rgba(219, 171, 87, 0.1) !important;
+    color: #DBAB57 !important;
+  }
+  
+  /* Active menu (khi đang ở trang đó) */
+  .router-link-active {
+    background-color: rgba(219, 171, 87, 0.15) !important;
+    color: #DBAB57 !important;
+    font-weight: 600;
+  }
+  
+  /* Dropdown menu đẹp hơn */
+  .dropdown-menu {
+    border: none;
+    box-shadow: 0 10px 30px rgba(0,0,0,0.1);
+    border-radius: 12px;
+  }
+  
+  .dropdown-item {
+    padding: 10px 20px;
+    border-radius: 8px;
+    margin: 4px 8px;
+  }
+  
+  /* Mobile menu vẫn hoạt động bình thường */
+  .mobile-topbar-header {
+    background: #1a1a1a;
+    color: white;
+  }
+  </style>
--- src/layout/components/TopRocker.vue
+++ src/layout/components/TopRocker.vue
@@ -1,98 +1,158 @@
 <template>
     <header>
-        <div class="topbar d-flex align-items-center">
-            <nav class="navbar navbar-expand">
-                <div class="topbar-logo-header">
-                    <div class="">
-                        <img src="https://i.pinimg.com/736x/cc/55/23/cc55235082b0cbee0f53587c11278fb2.jpg" class="logo-icon" alt="logo icon">
-                    </div>
-                    <div class="">
-                        <h4 class="logo-text">Hotel</h4>
-                    </div>
+      <div class="topbar d-flex align-items-center">
+        <nav class="navbar navbar-expand w-100">
+          <!-- Logo chữ AIHOTEL bên trái -->
+          <div class="d-flex align-items-center">
+            <h4 class="logo-text mb-0">AIHOTEL</h4>
+          </div>
+  
+          <!-- Nút menu mobile -->
+          <div class="mobile-toggle-menu d-lg-none ms-auto me-3">
+            <i class='bx bx-menu fs-3'></i>
+          </div>
+  
+          <!-- User dropdown ở góc phải -->
+          <div class="ms-auto d-flex align-items-center">
+            <div class="user-box dropdown">
+              <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#"
+                 role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg" 
+                     class="user-img rounded-circle" alt="Admin">
+                <div class="user-info ps-3 text-start">
+                  <p class="user-name mb-0 fw-semibold">{{ ten_hien_thi }}</p>
+                  <p class="designattion mb-0 text-muted small">Quản trị viên</p>
                 </div>
-                <div class="mobile-toggle-menu"><i class='bx bx-menu'></i></div>
-                <div class="search-bar flex-grow-1">
-                    <div class="position-relative search-bar-box">
-                        <input type="text" class="form-control search-control" placeholder="Type to search..."> <span
-                            class="position-absolute top-50 search-show translate-middle-y"><i
-                                class='bx bx-search'></i></span>
-                        <span class="position-absolute top-50 search-close translate-middle-y"><i
-                                class='bx bx-x'></i></span>
-                    </div>
-                </div>
-                <div class="top-menu ms-auto">
-                    <ul class="navbar-nav align-items-center">
-                        <li class="nav-item mobile-search-icon">
-                            <a class="nav-link" href="#"> <i class='bx bx-search'></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="user-box dropdown">
-                    <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img v-bind:src="avatar" class="user-img" alt="user avatar">
-                        <div class="user-info ps-3">
-                            <p class="user-name mb-0">{{ ten_hien_thi }}</p>
-                            <p class="designattion mb-0">Admin</p>
-                        </div>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a v-on:click="dangXuat()" class="dropdown-item"><i
-                                    class='bx bx-log-out-circle'></i><span>Đăng Xuất</span></a>
-                        </li>
-                        <li><a v-on:click="dangXuatAll()" class="dropdown-item"><i
-                                    class='bx bx-log-out-circle'></i><span>Đăng Xuất Tất Cả</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end shadow-lg border-0">
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" @click="dangXuat()">
+                    <i class='bx bx-log-out-circle me-2'></i><span>Đăng xuất</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex align-items-center" @click="dangXuatAll()">
+                    <i class='bx bx-log-out-circle me-2'></i><span>Đăng xuất tất cả thiết bị</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </header>
-</template>
-<script>
-import axios from 'axios';
-import { createToaster } from "@meforma/vue-toaster";
-import baseRequest from '../../core/baseRequest';
-const toaster = createToaster({ position: "top-right" });
-export default {
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  import { createToaster } from "@meforma/vue-toaster";
+  import baseRequest from '../../core/baseRequest';
+  const toaster = createToaster({ position: "top-right" });
+  
+  export default {
     data() {
-        return {
-            ten_hien_thi: 'Chưa đăng nhập',
-            avatar: 'Chưa đăng nhập',
-        }
+      return {
+        ten_hien_thi: 'Chưa đăng nhập',
+        avatar: 'Chưa đăng nhập',
+      }
     },
     mounted() {
-        this.checkLogin();
+      this.checkLogin();
     },
     methods: {
-        checkLogin() {
-            baseRequest
-                .post('kiem-tra-token-admin', {})
-                .then((res) => {
-                    if (res.data.status) {
-                        this.ten_hien_thi = localStorage.getItem('ho_ten_admin');
-                        this.avatar = localStorage.getItem('avatar_admin');
-                    }
-                });
-        },
-        dangXuat() {
-            baseRequest
-                .get('admin/dang-xuat')
-                .then((res) => {
-                    toaster.success(res.data.message);
-                    this.$router.push('/admin/dang-nhap');
-                })
-        },
-        dangXuatAll() {
-            baseRequest
-                .get('admin/dang-xuat-all')
-                .then((res) => {
-                    toaster.success(res.data.message);
-                    this.$router.push('/admin/dang-nhap');
-                })
-        }
+      checkLogin() {
+        baseRequest
+          .post('kiem-tra-token-admin', {})
+          .then((res) => {
+            if (res.data.status) {
+              this.ten_hien_thi = localStorage.getItem('ho_ten_admin') || 'Admin';
+              this.avatar = localStorage.getItem('avatar_admin');
+            }
+          });
+      },
+      dangXuat() {
+        baseRequest.get('admin/dang-xuat').then((res) => {
+          toaster.success(res.data.message);
+          this.$router.push('/admin/dang-nhap');
+        });
+      },
+      dangXuatAll() {
+        baseRequest.get('admin/dang-xuat-all').then((res) => {
+          toaster.success(res.data.message);
+          this.$router.push('/admin/dang-nhap');
+        });
+      }
     },
-}
-</script>
-<style></style>
+  }
+  </script>
+  
+  <style scoped>
+  /* Nền trắng + viền dưới nhẹ */
+  .topbar {
+    background: #ffffff !important;
+    border-bottom: 1px solid #e9ecef;
+    padding: 0 20px;
+    height: 70px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+  }
+  
+  /* Logo chữ AIHOTEL */
+  .logo-text {
+    font-size: 28px;
+    font-weight: 800;
+    color: #DBAB57 !important;
+    letter-spacing: 1px;
+    font-family: 'Playfair Display', Georgia, serif;
+  }
+  
+  /* Ảnh admin - kích thước vừa mắt, không viền */
+  .user-img {
+    width: 48px;
+    height: 48px;
+    object-fit: cover;
+    border-radius: 50%;
+    box-shadow: 0 4px 12px rgba(0,0,0,0.1);
+  }
+  
+  /* Ẩn hoàn toàn thanh tìm kiếm */
+  .search-bar,
+  .mobile-search-icon,
+  .top-menu {
+    display: none !important;
+  }
+  
+  /* Dropdown menu đẹp */
+  .dropdown-menu {
+    border: none;
+    border-radius: 12px;
+    padding: 8px 0;
+    min-width: 220px;
+    box-shadow: 0 10px 30px rgba(0,0,0,0.15);
+  }
+  
+  .dropdown-item {
+    padding: 10px 20px;
+    font-size: 15px;
+    border-radius: 8px;
+    margin: 0 8px;
+  }
+  
+  .dropdown-item:hover {
+    background: #f8f9fa;
+    color: #DBAB57;
+  }
+  
+  /* Responsive */
+  @media (max-width: 991px) {
+    .logo-text {
+      font-size: 24px;
+    }
+    .user-info {
+      display: none;
+    }
+    .user-img {
+      width: 42px;
+      height: 42px;
+    }
+  }
+  </style>
